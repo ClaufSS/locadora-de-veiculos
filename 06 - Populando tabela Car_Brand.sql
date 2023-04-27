4c9f471e10e147451259f6d6146b19ee0f9bccf6
clauf <claufsts@gmail.com>
2023-04-27 14:20:33 -0300

    Criando tabela Car_Brand

---
+++ 06 - Populando tabela Car_Brand.sql
@@ -0,0 +1,15 @@
+-- SQLite
+
+-- Populando tabela CAR_BRAND (marcas)
+INSERT INTO CAR_BRAND (BRAND_NAME)
+  VALUES ('Chevrolet'),
+         ('Toyota'),
+         ('Hyundai'),
+         ('Volkswagen'),
+         ('Jeep'),
+         ('Renault'),
+         ('Honda'),
+         ('Fiat');
+
+-- Exibindo tabela CAR_BRAND
+SELECT * FROM CAR_BRAND;
