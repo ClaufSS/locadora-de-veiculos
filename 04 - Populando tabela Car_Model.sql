0f141928dd64e4f1d25afd68bc4b433f3d52f14a
clauf <claufsts@gmail.com>
2023-04-27 14:09:48 -0300

    Criando tabela Car_Model

---
+++ 04 - Populando tabela Car_Model.sql
@@ -0,0 +1,18 @@
+-- SQLite
+
+-- Populando tabela CAR_MODEL (modelos)
+INSERT INTO CAR_MODEL (MODEL_NAME)
+  VALUES ('Conversível'),
+         ('Sedã'),
+         ('Hatch'),
+         ('Coupé'),
+         ('Perua'),
+         ('SUV'),
+         ('Picape'),
+         ('Minivan'),
+         ('Utilitário'),
+         ('Buggy');
+
+
+-- Exibindo tabela CAR_MODEL
+-- SELECT * FROM CAR_MODEL;
