f1fcd201e242d829806ab7c5fa6430320dc43744
clauf <claufsts@gmail.com>
2023-04-29 10:55:43 -0300

    Consulta em LOCATIONS com filtro de intervalo cronológico

---
+++ 25 - Consulta com filtro de intervalo cronológico.sql
@@ -0,0 +1,9 @@
+-- SQLite
+
+
+-- Consultas com função de agregação
+-- Consulta com filtro de intervalo cronológico
+
+SELECT * FROM LOCATIONS 
+  WHERE START_DATE BETWEEN '2022-05-20' AND '2022-12-25';
+
